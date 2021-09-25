c6e61b649ddcdca87d01b52553a0cb009785027a
ma-tesi <79223382+ma-tesi@users.noreply.github.com>
2021-09-25 19:30:58 +0200

    Update test.py

--- Python code/test.py
+++ Python code/test.py
@@ -7,28 +7,28 @@ from plot_confusion_matrix import plot_confusion_matrix
 import tensorflow as tf
 import time
 
-MOBILE_CLASSIC_MODEL = 'mobile_handsRGB_model.h5'  # unica pecca sul 9
-MOBILE_NEW_AUG = 'newAUG_mobile_handsRGB_model.h5'  # perfetta senza la riduzione di 1./255
+MOBILE_CLASSIC_MODEL = 'mobile_handsRGB_model.h5'
+MOBILE_NEW_AUG = 'newAUG_mobile_handsRGB_model.h5'
 
-Half_trainable = 'Half_trainable.h5'  #pecca sul 6, perfetta se non si scala il /255
-VGG_CLASSIC_MODEL = 'VGG_handsRGB_model.h5'     # peggio della two steps
-Bigdata_SQUEEZE = 'bigdata_simple_squeeze.h5'  #dipende,meglio senza il /255 incerta tra quattro e 6
+Half_trainable = 'Half_trainable.h5'
+VGG_CLASSIC_MODEL = 'VGG_handsRGB_model.h5'
+Bigdata_SQUEEZE = 'bigdata_simple_squeeze.h5'
 
 SQUEEZE_2STEPS = 'squeezenet_twosteps.h5'  #
 SQUEEZE_FT = 'Squeeze_FT_imagenet_AUG.h5'
 SQUEEZE_CLASSIC = 'classic_squeeze.h5'
-SQUEEZE_SIMPLE = 'simple_squeeze.h5' #buona, ha problei sui numeri centrali come conferma la confusion mat
+SQUEEZE_SIMPLE = 'simple_squeeze.h5'
 SQUEEZE_64 = 'simple_squeeze_64batches.h5'
 SQ32 = 'SQ32best_model.h5'
 simple32 = 'simple_squeeze.h5'
 
 BD_SQ1 = 'BDclassic_squeeze.h5'
-BD_SQ2 = 'BDSqueeze_FT_imagenet_AUG.h5' # migliore
+BD_SQ2 = 'BDSqueeze_FT_imagenet_AUG.h5'
 BD_SQ3 = 'BDsqueeze_reduced_imagenet.h5'
 BD_SQ4 = 'SQ_best_model.h5'
 BD_SQ16 = 'SQ16.h5'
 BD_SQ64 = 'SQ64_best_model.h5'
-BD_SQ = 'BDsimple_squeeze.h5'  # migliorabile
+BD_SQ = 'BDsimple_squeeze.h5'
 BD_MOB = 'BDmobile_handsRGB_model.h5'
 BD_MOB4='bigdata_mobile_handsRGB_model.h5'
 BD_MOB1 = 'Mobile_best_model.h5'
@@ -42,12 +42,9 @@ Full_trainable = 'VGG16_Full_trainable.h5'
 BD_VGG2 = 'BD_AUG_VGGtwosteps_model.h5'
 BD_VGG3 = 'BD_VGG_model.h5'
 Augmented = 'Augmented_VGG16.h5'
-#MODELS = [MOBILE_CLASSIC_MODEL, MOBILE_NEW_AUG, VGG_AUG_MODEL, BD_MOB, BD_SQ, Bigdata_SQUEEZE, SQUEEZE_64]
-#MODELS = [BD_SQ2, BD_SQ64, BD_SQ16]
-#MODELS = [BD_MOB1, BD_MOB, BD_MOB2]
 
 #BEST OF
-#MODELS = [BD_SQ16, BD_MOB2, BD_MOB, Full_trainable]
+MODELS = [BD_SQ16, BD_MOB2, BD_MOB, Full_trainable]
 
 #trying VGG16
 #MODELS = [Full_trainable, Half_trainable, Augmented]
@@ -56,9 +53,8 @@ Augmented = 'Augmented_VGG16.h5'
 #MODELS = [BD_SQ16, BD_SQ, SQ32]
 
 #optimizers
-MODELS = [MobRMS2, MobSGD]
-#  Mobile
-#  MODELS = [BD_MOB2, BD_MOB4, MOBILE_CLASSIC_MODEL, BD_MOB]
+#MODELS = [MobRMS2, MobSGD]
+
 x = pickle.load(open('X_test.pickle', 'rb'))
 y = pickle.load(open('y_test.pickle', 'rb'))
 
@@ -94,8 +90,7 @@ for mod in MODELS:
 plt.show()
 
 
-'''from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
-plt.imshow(x_test[7, :, :, :].squeeze(), interpolation='nearest')
-plt.show()
-'''
+#plt.imshow(x_test[7, :, :, :].squeeze(), interpolation='nearest')
+#plt.show()
